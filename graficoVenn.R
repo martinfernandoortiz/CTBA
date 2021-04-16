9d4f5b8851b3b328490a2122928469dd73d99ff7
martinfernandoortiz <38224115+martinfernandoortiz@users.noreply.github.com>
2021-04-16 11:55:11 -0300

    Add files via upload
    Gráfico de Venn para explicar la recurrencia al complejo

---
+++ graficoVenn.R
@@ -0,0 +1,24 @@
+ install.packages("ggVennDiagram")
+library(ggVennDiagram)
+library(tidyverse)
+
+ 
+ library(readr)
+ v_ctba <- read_delim("C:/Users/GCBA/Desktop/v_ctba.txt", 
+                      "\t", escape_double = FALSE, col_types = cols(comprasporpersona = col_character(), 
+                                                                    documento = col_character()), trim_ws = TRUE)
+recurrencia1 <- v_ctba %>% filter(comprasporpersona=='1')
+recurrencia2 <- v_ctba %>% filter(comprasporpersona=='2')
+recurrencia3 <- v_ctba %>% filter(comprasporpersona=='3')
+recurrencia4 <- v_ctba %>% filter(comprasporpersona=='4 ó más')
+
+x <- list(A=recurrencia1$documento,
+          B=recurrencia2$documento,
+          C=recurrencia3$documento)#,
+          D=recurrencia4$documento)
+
+
+
+ggVennDiagram(x,category.names = c("Recurrencia 1",
+                                   "Recurrencia 2",
+                                   "Recurrencia 3"))
